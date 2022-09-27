72a39704c2cf59868ad2c9014ab6dd58663962d2
Alex\Aki <aglisic1717@gmail.com>
2022-09-27 20:33:45 +0200

    This is my  first commit

---
+++ JavaBasicOOP/src/loops/ForLoops.java
@@ -0,0 +1,10 @@
+package loops;
+
+public class ForLoops {
+
+    public void testForm() {
+        for (int i = 0; i < 2; i++) {
+            System.out.println("This is for loop" + "  " + i);
+        }
+    }
+}
