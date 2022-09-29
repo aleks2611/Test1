fd1c6d5171123b388fbac78688c7d249e49f69a1
Alex\Aki <aglisic1717@gmail.com>
2022-09-29 20:31:39 +0200

    Exersize commits

---
+++ JavaBasicOOP/src/exersize/B.java
@@ -0,0 +1,13 @@
+package exersize;
+
+public class B {
+
+    public static void main(String[] args) {
+        A a = new A();
+
+    }
+
+    private void thinkAboutLife(int number) {
+        System.out.println("Aleksandra");
+    }
+}
